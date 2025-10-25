dd24771c4c8df7ea833dadada04a8e242435ca37
Ankit Dubey <codeexciteankit@gmail.com>
2025-10-25 22:23:29 +0530

    Update numpy-Basics.py
    note

--- ML-codes/python-basics/python-libs/numpy-Basics.py
+++ ML-codes/python-basics/python-libs/numpy-Basics.py
@@ -1,3 +1,26 @@
-import numpy as np
-arr = np.array([1, 2, 3, 4, 5])
-print(arr)
+🔥 What is NumPy?
+
+NumPy (Numerical Python) is a high-performance numerical computation library that:
+
+Replaces Python lists for mathematical & data-heavy operations
+
+Makes Python run 10x–100x faster
+
+Is the foundation of Machine Learning, AI, Data Science, Image Processing, Finance, and even DSA optimization
+
+
+Without NumPy → You can’t become industry-level in Python.
+
+
+---
+
+🚀 Why NumPy is Faster than Python List?
+
+Python List	NumPy Array
+
+Stores data randomly	Stores data in continuous memory (C)
+Slow in loops	Super fast (no loops needed)
+Python-level	C-level (machine optimized)
+Bad for big data	Made for AI, ML, Data, Performance
+
+
